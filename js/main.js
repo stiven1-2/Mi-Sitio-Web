4a7f2da282ef6a5a254c61068a8c19d21812a22a
Kevin Ijaji <kevinstivenijaji9@gmail.com>
2021-09-04 15:11:58 -0500

    add menu-responsive

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
-$(document).ready(main);
+ $(document).ready(main);
 
 var contador = 1;
 
 function main() {
-    $('.menu-bar').click(function(){
-        if (contador ==1)
+    $('.menu-bar').click(function() {
+        if (contador == 1){
         $('nav').animate ({
-            left:0
+             left:'0'
+        });
+        contador = 0;
         }
-    contador = 0;
-    }
-        else {
-       contador = 1;
-       $('nav').animate ({
-           left:'-100%'
-       });
-    }    
-});
-$('.submenu').click(function()
+       else {
+        $('nav').animate ({
+            left:'-100%' 
+        });
+        contador = 0;
+        }
+        });
+        $('.submenu').click(function(){
+        $(this).children('.children').slideToggle();
+     });
+}
