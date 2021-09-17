fd1277166418a0088b455bfc87a301c41d9e3c0a
Kevin Ijaji <kevinstivenijaji9@gmail.com>
2021-09-17 11:02:32 -0500

    add menu responsive

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
  $(document).ready(main);
 
-var contador = 1;
+ var contador = 1;
 
-function main() {
-    $('.menu-bar').click(function() {
-        if (contador == 1){
-        $('nav').animate ({
-             left:'0'
-        });
-        contador = 0;
-        }
-       else {
-        $('nav').animate ({
-            left:'-100%' 
-        });
-        contador = 0;
-        }
-        });
-        $('.submenu').click(function(){
-        $(this).children('.children').slideToggle();
+ function main() {
+     $('.menu-bar').click(function() {
+         if (contador == 1) {
+             $('nav').animate({
+                 left: '0'
+             });
+             contador = 0;
+         } else {
+             $('nav').animate({
+                 left: '-100%'
+             });
+             contador = 0;
+         }
      });
-}
+     $('.submenu').click(function() {
+         $(this).children('.children').slideToggle();
+     });
+ }
