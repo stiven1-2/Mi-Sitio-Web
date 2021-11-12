552dea52e9eea10a56687bf7e9627739e686e411
Kevin <kevinstivenijaji9@gmail.com>
2021-11-12 17:44:44 -0500

    add javascrip

---
+++ javascrip/js/2-useStrict.js
@@ -0,0 +1,6 @@
+/*Variable Declarada o inicializada*/ 
+var nombreAprendiz1 = "kevin"
+/*No se crea la variable */
+nombreAprendiz2 = "stiven"
+console.log(nombreAprendiz1);
+console.log(nombreAprendiz2);
---
+++ javascrip/js/8-condicional-if.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var user =stiven1-2 ;
+var passsword = 12345;
+
+var usuario =stiven;
+var contraseña = 12345;
+if (user === contraseña && pass === contraseña){
+    console.log("bienvenidos");
+}
+else {
+    console.log("usuario y/o clabe incorrecta.");
+}
