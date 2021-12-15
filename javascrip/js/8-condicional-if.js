a59b2a1e1960c86c7ce37eebe2f4f10f5e736249
Kevin <kevinstivenijaji9@gmail.com>
2021-12-15 17:54:57 -0500

    add javascrip

--- javascrip/js/8-condicional-if.js
+++ javascrip/js/8-condicional-if.js
@@ -1,6 +1,6 @@
 "use strict"
 
-<<<<<<< HEAD
+
 var user_BD = "stiven";
 var pass_BD = 12345;
 
@@ -11,16 +11,15 @@ if (user_BD === user_input && pass_BD === pass_BD === pass_input) {
 } else {
     console.log("Bienvenidos");
 }
-=======
-var user ="stiven" ;
+
+var user = "stiven";
 var pass = 12345;
 
-var userBD ="stiven";
+var userBD = "stiven";
 var passBD = 12345;
-if (user === userBD && pass === passBD){
+if (user === userBD && pass === passBD) {
     console.log("Bienvenidos");
-}
-else {
+} else {
     console.log("Bienvenidos");
 }
 
@@ -29,30 +28,29 @@ else {
 
 var edadUsuario = 9;
 var destinoUsuario = "Cartagena";
-var aerolineaUsuario ="Latam";
+var aerolineaUsuario = "Latam";
 var destinoDescuento = "cartagena"
 var aerolineaDescuento = "Latam"
-var costoTiquete = "250000"; 
+var costoTiquete = "250000";
 var descuento = .10;
 var costoFinal = 0;
 var descuentousuario = ""
- 
+
 if (edadUsuario < 10 || edadUsuario > 70) {
     costoTiquete = costoTiquete - (costoTiquete * descuento);
     descuentousuario = "por edad";
     if (destinoUsuario === destinoDescuento)
-    costoTiquete = costoTiquete - (costoTiquete * descuento)
+        costoTiquete = costoTiquete - (costoTiquete * descuento)
     descuentousuario += descuentousuario + ", Por Destino"
 }
 if (aerolineaUsuario === aerolineaDescuento) {
     costoTiquete = costoTiquete - (costoTiquete * descuento)
     descuentousuario += " y por aerolinea"
 }
-if (descuentousuario !="") {
+if (descuentousuario != "") {
     console.log("Uste recibio un descuento por:" + descuentousuario);
     console.log("El valor final del tiquete con descuento es: $" + costoTiquete)
-}
-else {
+} else {
     console.log("El valor final del tiquete es: $" + costoTiquete)
     console.log("Lo sentimos, no aplica para ningun descuento")
 }
@@ -62,5 +60,3 @@ var edadMaxima = 140;
 if (edadCiudadano >= edadMinima && edadCiudadano <= edadMaxima) {
     console.log("Bienvenido. Usted cumples las condiciones")
 }
- 
->>>>>>> 9fbb4c3705dcc673a63a2eff52124320695b9aff
